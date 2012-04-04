e81ec30a1f70171ce0caf0f2f054e11795d157bd
James Reeves <jreeves@weavejester.com>
2012-04-04 23:59:20 +0100

    Released 0.2.0

--- environ.lein/project.clj
+++ environ.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject environ/environ.lein "0.2.0-SNAPSHOT"
+(defproject environ/environ.lein "0.2.0"
   :description "Leiningen plugin for Environ"
   :url "https://github.com/weavejester/environ"
   :eval-in-leiningen true)
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject environ "0.2.0-SNAPSHOT"
-  :description "Library for managing environment variables"
+(defproject environ "0.2.0"
+  :description "Library for managing environment settings"
   :url "https://github.com/weavejester/environ"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[environ/environ.core "0.2.0-SNAPSHOT"]])
+  :dependencies [[environ/environ.core "0.2.0"]])

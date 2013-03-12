b035deb901ec9e0e2ee7e665bfacb1aecde58622
James Reeves <jreeves@weavejester.com>
2013-03-12 14:00:13 +0000

    Added licenses

--- lein-environ/project.clj
+++ lein-environ/project.clj
@@ -1,4 +1,6 @@
 (defproject lein-environ "0.4.0-SNAPSHOT"
   :description "Leiningen plugin for Environ"
   :url "https://github.com/weavejester/environ"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)

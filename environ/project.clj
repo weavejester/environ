1208b48c4d789c60eff0c24a0a9a0f6708302c04
James Reeves <jreeves@weavejester.com>
2013-03-12 14:00:13 +0000

    Added licenses

--- environ/project.clj
+++ environ/project.clj
@@ -1,4 +1,6 @@
 (defproject environ "0.4.0-SNAPSHOT"
   :description "Core environ library for use in applications"
   :url "https://github.com/weavejester/environ"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]])

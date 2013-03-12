44e4d95e383b7ff3faa1e8ab38b8c3e01ce013a1
James Reeves <jreeves@weavejester.com>
2013-03-12 13:53:31 +0000

    Renamed projects to integrate better with Lein 2.0.0

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject environ "0.3.1"
-  :description "Library for managing environment settings"
-  :url "https://github.com/weavejester/environ"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[environ/environ.core "0.3.1"]])

333f098e38d3bef4e08534a820e643c85d6bfb18
Anand Muthu <ananthmv@gmail.com>
2014-04-17 10:12:50 -0400

    bumped clojure version to 1.6.0

--- environ/project.clj
+++ environ/project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/weavejester/environ"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])

543c87644d09a6c2f6f9c8e1a274cf2ccf405496
James Reeves <jreeves@weavejester.com>
2014-04-20 21:19:54 +0100

    Added :scm keys to subprojects

--- environ/project.clj
+++ environ/project.clj
@@ -1,6 +1,7 @@
 (defproject environ "0.4.0"
   :description "Core environ library for use in applications"
   :url "https://github.com/weavejester/environ"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]])
--- lein-environ/project.clj
+++ lein-environ/project.clj
@@ -1,6 +1,7 @@
 (defproject lein-environ "0.4.0"
   :description "Leiningen plugin for Environ"
   :url "https://github.com/weavejester/environ"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
